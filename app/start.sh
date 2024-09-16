ad26d9347585f8c33576f204046760a0d6dbcb4c
Devani Ayush <ayushdevani0018@gmail.com>
2024-09-16 19:07:47 +0530

    up

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the Flask app
-gunicorn --bind 0.0.0.0:5000 app:app &
+# gunicorn --bind 0.0.0.0:5000 app:app &
 
 # Start the Rasa server
 rasa run --model /app/rasa/models --port 5005
