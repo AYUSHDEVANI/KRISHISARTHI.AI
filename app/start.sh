c3cc55add195484d77bf5513501571f9cfcdc838
Devani Ayush <ayushdevani0018@gmail.com>
2024-09-16 18:18:16 +0530

    update

--- app/start.sh
+++ app/start.sh
@@ -1,12 +1,3 @@
-# #!/bin/bash
-
-# # Start Rasa server in the background
-# rasa run -m /app/rasa/models --enable-api --cors '*' &
-
-# # Start Flask app
-# python /app/flask-app/app.py
-
-
 #!/bin/bash
 
 # Start the Flask app
