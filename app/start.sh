2f83d20a3f35df5bb53d9653b667ef95ef09bc66
Devani Ayush <ayushdevani0018@gmail.com>
2024-09-16 22:35:19 +0530

    u10

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Rasa server in the background
-rasa run --enable-api --cors "*" --port 5005 &
+rasa run --enable-api --cors "*" &
 
 # Start Flask app
-flask run --host=0.0.0.0 --port 5000
+exec flask run --host=0.0.0.0 --port=5000
