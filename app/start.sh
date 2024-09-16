5d57e7a91bc6fedb812855567a1ddbdf6b4f6ea0
Devani Ayush <ayushdevani0018@gmail.com>
2024-09-17 00:23:27 +0530

    u8

--- app/start.sh
+++ app/start.sh
@@ -1,21 +1,36 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Start Rasa server
-rasa run --enable-api --cors "*" &
+# # Start Rasa server
+# rasa run --enable-api --cors "*" &
 
-# Start Action server
-rasa run actions &
+# # Start Action server
+# rasa run actions &
 
-# Start Flask app
+# # Start Flask app
+# cd /app/flask-app
+# flask run --host=0.0.0.0 --port=5000 &
+# wait -n
+
+
+
+# # Start Flask application
+# # export FLASK_APP=/app/flask-app/app.py
+# # /app/venv/bin/flask run --host=0.0.0.0 --port=5000
+
+# # Exit when either Flask or Rasa stops
+# exit $?
+
+
+# Start the Flask app in the background
+echo "Starting Flask app..."
 cd /app/flask-app
 flask run --host=0.0.0.0 --port=5000 &
-wait -n
 
+# Start the Rasa action server in the background
+echo "Starting Rasa action server..."
+cd /app/rasa
+rasa run actions --actions actions &
 
-
-# Start Flask application
-# export FLASK_APP=/app/flask-app/app.py
-# /app/venv/bin/flask run --host=0.0.0.0 --port=5000
-
-# Exit when either Flask or Rasa stops
-exit $?
+# Start the Rasa server
+echo "Starting Rasa server..."
+rasa run --enable-api --cors "*" --debug
