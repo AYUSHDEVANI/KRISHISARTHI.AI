8c005f657f430d872de8fa1e02cc82c79a3013c6
Devani Ayush <ayushdevani0018@gmail.com>
2024-09-17 18:52:24 +0530

    u

--- app/start.sh
+++ app/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Start the Flask app in the background
-echo "Starting Flask app..."
-cd /app/flask-app
-flask run --host=0.0.0.0 --port=5000 &
+# echo "Starting Flask app..."
+# cd /app/flask-app
+# flask run --host=0.0.0.0 --port=5000 &
 
 # Start the Rasa action server in the background
 echo "Starting Rasa action server..."
