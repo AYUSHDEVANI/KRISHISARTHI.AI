effbb4268959d6cf720723d70419ba8864649150
Devani Ayush <ayushdevani0018@gmail.com>
2024-09-17 18:45:54 +0530

    a

--- app/start.sh
+++ app/start.sh
@@ -8,7 +8,7 @@ flask run --host=0.0.0.0 --port=5000 &
 # Start the Rasa action server in the background
 echo "Starting Rasa action server..."
 cd /app/rasa
-rasa run actions --port 5055 &
+rasa run actions &
 
 # Start the Rasa server
 echo "Starting Rasa server..."
